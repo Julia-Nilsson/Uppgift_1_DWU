4a33b8529b1da2c99a9912b601c2e3c49a13188e
Julia-Nilsson <jniilsson@live.se>
2020-02-05 11:24:44 +0100

    config done

---
+++ config/config.js
@@ -0,0 +1,2 @@
+const config = { databaseUrl: "mongodb+srv://Julia:Potatiskung14@cluster0-itxue.mongodb.net/test?retryWrites=true&w=majority" }
+module.exports = config;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Connection
+// Required npms & ejs
 const express = require("express"); 
 
 const mongoose =require("mongoose"); 
 
 const app = express(); 
- 
-mongoose.connect("mongodb+srv://Julia:Potatiskung14@cluster0-itxue.mongodb.net/test?retryWrites=true&w=majority");
+
+//connection setup
+const config = require("./config/config.js")
+mongoose.connect(config.databaseUrl);
