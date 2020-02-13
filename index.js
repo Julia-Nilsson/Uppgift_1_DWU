b386a8fe087bc70b3b8e376c90d70b86eca74c4d
Julia-Nilsson <jniilsson@live.se>
2020-02-13 18:34:56 +0100

    html-struktur på comments.ejs

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(sassMiddleware({
     })
     )
    
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs");
  
